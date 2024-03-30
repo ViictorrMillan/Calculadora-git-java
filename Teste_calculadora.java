a415a77336ac95d8866259bd0fcddd63923c476a
Victor Millan <victorhugomartinsmillan@hotmail.com>
2024-03-30 19:22:56 -0300

    test(git-java): teste da função somar

---
+++ Teste_calculadora.java
@@ -0,0 +1,18 @@
+
+import javax.swing.JOptionPane;
+
+public class Teste_calculadora { 
+    public static void main(String[] args) {
+        int [] valores = new int [2];
+       
+        String valor1String = JOptionPane.showInputDialog("Digite o primeiro número: ");
+        valores [0] = Integer.parseInt(valor1String);
+            
+        String valor2String = JOptionPane.showInputDialog("Digite o segundo número: ");
+        valores [1] = Integer.parseInt(valor2String);
+
+        int resultado = valores[0] + valores[1];
+
+        JOptionPane.showMessageDialog(null, "A soma dos números é: " + resultado);
+    }
+}
